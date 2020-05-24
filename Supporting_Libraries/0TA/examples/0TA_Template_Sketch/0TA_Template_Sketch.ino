d2d47878ec0c806e796fabbcc56c262871fe0760
Syber-Space <syberxspace@gmail.com>
2020-05-24 01:00:45 -0400

    Update 0TA_Template_Sketch.ino

--- Supporting_Libraries/0TA/examples/0TA_Template_Sketch/0TA_Template_Sketch.ino
+++ Supporting_Libraries/0TA/examples/0TA_Template_Sketch/0TA_Template_Sketch.ino
@@ -1,19 +1,83 @@
-#include <OTA.h>
-unsigned long entry;
+// SKETCH NAME by Nicholas Schwankl (Syber-Space)
+// Uses 0TA (if OTA enabled), and LIBRARIES.
+// Copyright Statement
+
+//++++++++++ PRE-PROCESSOR OPTIONS ++++++++++//
+
+#define DEBUG true  //enables Serial and debugging
+#define OTA   true  //enables OTA
+
+char NAME[] = "Template Sketch";
+
+//+++++++++++++ PIN ASSIGNMENTS +++++++++++++//
+
+
+
+//+++++++++++++ GENERAL OPTIONS +++++++++++++//
+
+
+
+//+++++++++++++++ END OPTIONS +++++++++++++++//
+
+#if OTA
+  #include <OTA.h>
+#endif
+
+#define stackSize   1024*8
+TaskHandle_t Loop2;
+
+
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Booting");
 
-  ArduinoOTA.setHostname("TemplateSketch");
-  setupOTA();
+  #if DEBUG
+    Serial.begin(115200);
+    Serial.println((String)"Booting - [DEBUG: TRUE] [OTA: " + (OTA ? "True" : "False") + "]");
+    delay(100);
+  #endif
 
-  // your code
+  #if OTA
+    ArduinoOTA.setHostname(NAME);
+    setupOTA();
+    delay(100);
+  #endif
+  
+  xTaskCreatePinnedToCore(loop2, "Loop2", stackSize, NULL, 1, &Loop2, 0);
+  delay(500);
+
+//+++++++++++++++ BEGIN SETUP +++++++++++++++//
+  
+  
+  
+//++++++++++++++++ END SETUP ++++++++++++++++//
 
 }
 
-void loop() {
-  ArduinoOTA.handle();
-  
-  // your code here
 
+void loop() {
+  
+//+++++++++++++ BEGIN MAIN LOOP +++++++++++++//
+  
+  
+  
+//++++++++++++++ END MAIN LOOP ++++++++++++++//
+
+}
+
+
+void loop2(void * parameter) {
+  for(;;) {
+
+    #if OTA
+      ArduinoOTA.handle();
+    #endif
+
+//++++++++++++ BEGIN SECOND LOOP ++++++++++++//
+    
+    
+    
+//+++++++++++++ END SECOND LOOP +++++++++++++//
+    
+    vTaskDelay(1);
+    
+  }
 }
